2380382f691cdfd87aabb82a3724064e461e6d44
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2022-04-04 17:40:50 +0500

    Initial commit

---
+++ src/test/java/uz/pdp/rabbitmqserver/RabbitMqServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.rabbitmqserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
